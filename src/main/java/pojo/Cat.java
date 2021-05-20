62990e1f21d2325a3f6b15bbd786c698bd76cb96
Alexandr <guravskiy79@gmail.com>
2021-05-20 14:59:39 +0300

    first commit

---
+++ src/main/java/pojo/Cat.java
@@ -0,0 +1,13 @@
+package pojo;
+
+public class Cat {
+    private String say;
+
+    public String getSay() {
+        return this.say;
+    }
+
+    public void setSay(String say) {
+        this.say = say;
+    }
+}
